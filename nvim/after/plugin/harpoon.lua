c384647c445d175afe8c138eb401e4b4984d2810
srares76 <72464868+srares76@users.noreply.github.com>
2024-04-19 21:39:18 +0300

    remove commented code

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,21 +1,3 @@
--- local mark = require("harpoon.mark")
--- local ui = require("harpoon.ui")
---
--- vim.keymap.set("n", "<leader>a", mark.add_file)
--- vim.keymap.set("n", "<leader><leader>a", ui.toggle_quick_menu)
---
--- local function goAndCloseTree(num)
---   vim.cmd("NvimTreeClose")
---   ui.nav_file(num)
--- end
---
--- vim.keymap.set("n", "<leader>1", function() goAndCloseTree(1) end)
--- vim.keymap.set("n", "<leader>2", function() goAndCloseTree(2) end)
--- vim.keymap.set("n", "<leader>3", function() goAndCloseTree(3) end)
--- vim.keymap.set("n", "<leader>4", function() goAndCloseTree(4) end)
--- vim.keymap.set("n", "<leader>5", function() goAndCloseTree(5) end)
-
-
 local harpoon = require("harpoon")
 
 harpoon:setup()
@@ -34,7 +16,3 @@ vim.keymap.set("n", "<leader>2", function() goAndCloseTree(2) end)
 vim.keymap.set("n", "<leader>3", function() goAndCloseTree(3) end)
 vim.keymap.set("n", "<leader>4", function() goAndCloseTree(4) end)
 vim.keymap.set("n", "<leader>5", function() goAndCloseTree(5) end)
-
--- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
