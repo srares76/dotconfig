a93ad63546b1da151841769415f14a180c3e24d4
srares76 <72464868+srares76@users.noreply.github.com>
2024-05-31 16:44:19 +0300

    Neogit and undotree

---
+++ nvim/after/plugin/neogit.lua
@@ -0,0 +1,32 @@
+local neogit = require('neogit')
+
+neogit.setup {
+    disable_commit_confirmation = true,
+    telescope_sorter = function()
+        return require("telescope").extensions.fzf.native_fzf_sorter()
+    end,
+    integrations = {
+        telescope = true,
+        diffview = true
+    }
+}
+
+vim.keymap.set("n", "gs", function()
+    neogit.open({ kind = "vsplit" })
+end)
+
+vim.keymap.set("n", "<leader>gc", ":Neogit commit<CR>",
+    { silent = true, noremap = true }
+)
+
+vim.keymap.set("n", "<leader>gp", ":Neogit pull<CR>",
+    { silent = true, noremap = true }
+)
+
+vim.keymap.set("n", "<leader>gP", ":Neogit push<CR>",
+    { silent = true, noremap = true }
+)
+
+vim.keymap.set("n", "<leader>gb", ":Telescope git_branches<CR>",
+    { silent = true, noremap = true }
+)
